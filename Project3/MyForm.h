9588e2d03d1da789f5b14956a0467a95a713018c
Vasya Yasyuk <smoyukmekj@mail.ru>
2024-05-11 18:03:13 +0300

    Добавьте файлы проекта.

---
+++ Project3/MyForm.h
@@ -0,0 +1,461 @@
+﻿#pragma once
+#include <math.h>
+namespace Project3 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Сводка для MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^ button1;
+	protected:
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::TextBox^ textBox6;
+	private: System::Windows::Forms::TextBox^ textBox5;
+	private: System::Windows::Forms::Label^ label7;
+	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::Label^ label5;
+
+	private:
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox6 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
+			this->label7 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(26, 459);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(418, 43);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Показать таблицу и экстремумы функции";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(467, 459);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(135, 43);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"Выход";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(12, 27);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(284, 220);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox1->TabIndex = 16;
+			this->pictureBox1->TabStop = false;
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Location = System::Drawing::Point(312, 73);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->RowHeadersVisible = false;
+			this->dataGridView1->Size = System::Drawing::Size(299, 313);
+			this->dataGridView1->TabIndex = 17;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(159, 394);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(100, 20);
+			this->textBox4->TabIndex = 25;
+			this->textBox4->Text = L"4";
+			this->textBox4->Leave += gcnew System::EventHandler(this, &MyForm::textBox4_Leave);
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(159, 357);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(100, 20);
+			this->textBox3->TabIndex = 24;
+			this->textBox3->Text = L"0.23";
+			this->textBox3->Leave += gcnew System::EventHandler(this, &MyForm::textBox3_Leave);
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(159, 324);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(100, 20);
+			this->textBox2->TabIndex = 23;
+			this->textBox2->Text = L"2.5";
+			this->textBox2->Leave += gcnew System::EventHandler(this, &MyForm::textBox2_Leave);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(159, 290);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 20);
+			this->textBox1->TabIndex = 22;
+			this->textBox1->Text = L"-3.4";
+			this->textBox1->Leave += gcnew System::EventHandler(this, &MyForm::textBox1_Leave);
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(19, 397);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(114, 13);
+			this->label4->TabIndex = 21;
+			this->label4->Text = L"Введите значение а=";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(19, 360);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(123, 13);
+			this->label3->TabIndex = 20;
+			this->label3->Text = L"Введите значение XH=";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(19, 327);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(125, 13);
+			this->label2->TabIndex = 19;
+			this->label2->Text = L"Введите значение XK= ";
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(16, 293);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(123, 13);
+			this->label1->TabIndex = 18;
+			this->label1->Text = L"Введите значение XN=";
+			// 
+			// textBox6
+			// 
+			this->textBox6->Location = System::Drawing::Point(467, 421);
+			this->textBox6->Name = L"textBox6";
+			this->textBox6->ReadOnly = true;
+			this->textBox6->Size = System::Drawing::Size(135, 20);
+			this->textBox6->TabIndex = 29;
+			// 
+			// textBox5
+			// 
+			this->textBox5->Location = System::Drawing::Point(321, 421);
+			this->textBox5->Name = L"textBox5";
+			this->textBox5->ReadOnly = true;
+			this->textBox5->Size = System::Drawing::Size(123, 20);
+			this->textBox5->TabIndex = 28;
+			// 
+			// label7
+			// 
+			this->label7->AutoSize = true;
+			this->label7->Location = System::Drawing::Point(464, 391);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(120, 13);
+			this->label7->TabIndex = 27;
+			this->label7->Text = L"Миним. знач. функции";
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Location = System::Drawing::Point(318, 394);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(126, 13);
+			this->label6->TabIndex = 26;
+			this->label6->Text = L"Максим. знач. функции";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(309, 27);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(146, 13);
+			this->label5->TabIndex = 30;
+			this->label5->Text = L"Таблица значений функции";
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(619, 514);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->textBox6);
+			this->Controls->Add(this->textBox5);
+			this->Controls->Add(this->label7);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->dataGridView1);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedToolWindow;
+			this->Name = L"MyForm";
+			this->Text = L"Выполнил студент группы 23-КФ Ясюк В. В.";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		double xn, xk, xh, x, y, a, ymax, ymin, yt;
+		int  i;
+		//Проверка ввода данных в компоненты textBox 
+		if ((textBox1->Text != "") && (textBox2->Text != "") &&
+			(textBox3->Text != "") && (textBox4->Text != ""))
+		{
+			xn = -3.4;
+			xk = 2.5;
+			xh =0.23;
+			a = Convert::ToDouble(textBox4->Text);
+			//Очистка столбцов таблицы
+			dataGridView1->Columns->Clear();
+			//Создание двух столбцов в таблице
+			dataGridView1->ColumnCount = 2;
+			//Создание в таблице строк 
+			dataGridView1->Rows->Add(ceil((xk - xn) / xh) + 1);
+			////Занесение в верхнюю строку таблицы в первую ячейку текст «Х», во вторую текст «У»
+			dataGridView1->Columns[0]->Name = " X";
+			dataGridView1->Columns[1]->Name = " Y";
+			i = 0;
+			x = xn;
+			ymax = -1.8e307; ymin = 1.8e307;
+			while (x <= xk)
+			{
+				if (x <= 0) { y = log10(fabs(x + 1)) + sqrt(3 * fabs(x)); }
+				else
+					if (x <= a) { y = sin(pow(x, 2) + 3 * x); }
+					else
+					{
+						y = pow(log10(fabs(x)), 2) + sqrt(x + 3);
+					}
+				//Занесение в первый столбец значений аргумента Х
+				dataGridView1->Rows[i]->Cells[0]->Value = Convert::ToString(x);
+				//Переменной yt присваивает округленное до двух знаков после запятой значение у
+				yt = ceil(y * 100) / 100;
+				//Вывод во втором столбце таблицы значение функции У 
+				dataGridView1->Rows[i]->Cells[1]->Value
+					= Convert::ToString(yt);
+				//находит максимальное и минимальное значение и округляет до двух знаков после запятой
+				if (y > ymax) ymax = ceil(y * 100) / 100;
+				if (y < ymin) ymin = ceil(y * 100) / 100;
+				x = x + xh;
+				i++;
+			}
+			//выводит в компоненты textbox максимальное и минимальное значение функции
+			textBox5->Text = Convert::ToString(ymax);
+			textBox6->Text = Convert::ToString(ymin);
+		}
+		else {
+			MessageBox::Show("Заполните, пожалуйста, данные",
+				"Ошибка ввода данных",
+				MessageBoxButtons::OK, MessageBoxIcon::Exclamation);
+		}
+
+	}
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	Application::Exit();
+}
+private: System::Void textBox1_Leave(System::Object^ sender, System::EventArgs^ e) {
+	int l, t, k; bool a = true; String^ str; str = textBox1->Text;
+	l = str->Length;
+	//èíäåêñ ñèìâîëà, ñ êîòîðûì ðàáîòàåì 
+	t = 0;
+	//êîëëè÷åñòâî çàïÿòûõ â ñòðîêå (äàáû èçáåæàòü âàðèàíòà 0, 2384, 1254, 1251  ýòî íå ÷èñëî) 
+	k = 0;
+	//äâèãàåì èíäåêñàòîð, åñëè íàøå ÷èñëî îòðèöàòåëüíîå 
+	if (str[t] == '-') { t++; }
+	//÷èñëî íå ìîæåò íà÷èíàòüñÿ ñ çàïÿòîé 
+	if (str[t] == ',' || str[t] == '.') { a = false; } while (t < l)
+	{
+		if (str[t] == ',' || str[t] == '.')
+			//åñëè çàïÿòàÿ ñòîèò ïîñëåäíèì ñèìâîëîì èëè çàïÿòàÿ óæå áûëà íàéäåíà 
+		{
+			if (t == l - 1 || k > 0) { a = false; } k++;
+		}
+		//åñëè t-ûé ñèìâîë íå ëåæèò â äèàïàçîíå îò '0' äî '9' 
+		else if (str[t] < '0' ||
+			str[t] > '9') {
+			a = false;
+		}
+		t++;
+	}
+	if (a == false)
+	{
+		MessageBox::Show("ïàðàìåòð À íå ÿâëÿåòñÿ ÷èñëîì", "Îøèáêà ââîäà äàííûõ", MessageBoxButtons::OK, MessageBoxIcon::Exclamation);
+		//âîçâðàò ôîêóñà òåêñòîâîìó ïîëþ 
+		this->textBox1->Focus();
+	}
+}
+private: System::Void textBox2_Leave(System::Object^ sender, System::EventArgs^ e) {
+	int l, t, k; bool a = true; String^ str; str = textBox1->Text;
+	l = str->Length;
+	//èíäåêñ ñèìâîëà, ñ êîòîðûì ðàáîòàåì 
+	t = 0;
+	//êîëëè÷åñòâî çàïÿòûõ â ñòðîêå (äàáû èçáåæàòü âàðèàíòà 0, 2384, 1254, 1251  ýòî íå ÷èñëî) 
+	k = 0;
+	//äâèãàåì èíäåêñàòîð, åñëè íàøå ÷èñëî îòðèöàòåëüíîå 
+	if (str[t] == '-') { t++; }
+	//÷èñëî íå ìîæåò íà÷èíàòüñÿ ñ çàïÿòîé 
+	if (str[t] == ',' || str[t] == '.') { a = false; } while (t < l)
+	{
+		if (str[t] == ',' || str[t] == '.')
+			//åñëè çàïÿòàÿ ñòîèò ïîñëåäíèì ñèìâîëîì èëè çàïÿòàÿ óæå áûëà íàéäåíà 
+		{
+			if (t == l - 1 || k > 0) { a = false; } k++;
+		}
+		//åñëè t-ûé ñèìâîë íå ëåæèò â äèàïàçîíå îò '0' äî '9' 
+		else if (str[t] < '0' ||
+			str[t] > '9') {
+			a = false;
+		}
+		t++;
+	}
+	if (a == false)
+	{
+		MessageBox::Show("ïàðàìåòð À íå ÿâëÿåòñÿ ÷èñëîì", "Îøèáêà ââîäà äàííûõ", MessageBoxButtons::OK, MessageBoxIcon::Exclamation);
+		//âîçâðàò ôîêóñà òåêñòîâîìó ïîëþ 
+		this->textBox1->Focus();
+	}
+}
+private: System::Void textBox3_Leave(System::Object^ sender, System::EventArgs^ e) {
+	int l, t, k; bool a = true; String^ str; str = textBox1->Text;
+	l = str->Length;
+	//èíäåêñ ñèìâîëà, ñ êîòîðûì ðàáîòàåì 
+	t = 0;
+	//êîëëè÷åñòâî çàïÿòûõ â ñòðîêå (äàáû èçáåæàòü âàðèàíòà 0, 2384, 1254, 1251  ýòî íå ÷èñëî) 
+	k = 0;
+	//äâèãàåì èíäåêñàòîð, åñëè íàøå ÷èñëî îòðèöàòåëüíîå 
+	if (str[t] == '-') { t++; }
+	//÷èñëî íå ìîæåò íà÷èíàòüñÿ ñ çàïÿòîé 
+	if (str[t] == ',' || str[t] == '.') { a = false; } while (t < l)
+	{
+		if (str[t] == ',' || str[t] == '.')
+			//åñëè çàïÿòàÿ ñòîèò ïîñëåäíèì ñèìâîëîì èëè çàïÿòàÿ óæå áûëà íàéäåíà 
+		{
+			if (t == l - 1 || k > 0) { a = false; } k++;
+		}
+		//åñëè t-ûé ñèìâîë íå ëåæèò â äèàïàçîíå îò '0' äî '9' 
+		else if (str[t] < '0' ||
+			str[t] > '9') {
+			a = false;
+		}
+		t++;
+	}
+	if (a == false)
+	{
+		MessageBox::Show("ïàðàìåòð À íå ÿâëÿåòñÿ ÷èñëîì", "Îøèáêà ââîäà äàííûõ", MessageBoxButtons::OK, MessageBoxIcon::Exclamation);
+		//âîçâðàò ôîêóñà òåêñòîâîìó ïîëþ 
+		this->textBox1->Focus();
+	}
+}
+private: System::Void textBox4_Leave(System::Object^ sender, System::EventArgs^ e) {
+	int l, t, k; bool a = true; String^ str; str = textBox1->Text;
+	l = str->Length;
+	//èíäåêñ ñèìâîëà, ñ êîòîðûì ðàáîòàåì 
+	t = 0;
+	//êîëëè÷åñòâî çàïÿòûõ â ñòðîêå (äàáû èçáåæàòü âàðèàíòà 0, 2384, 1254, 1251  ýòî íå ÷èñëî) 
+	k = 0;
+	//äâèãàåì èíäåêñàòîð, åñëè íàøå ÷èñëî îòðèöàòåëüíîå 
+	if (str[t] == '-') { t++; }
+	//÷èñëî íå ìîæåò íà÷èíàòüñÿ ñ çàïÿòîé 
+	if (str[t] == ',' || str[t] == '.') { a = false; } while (t < l)
+	{
+		if (str[t] == ',' || str[t] == '.')
+			//åñëè çàïÿòàÿ ñòîèò ïîñëåäíèì ñèìâîëîì èëè çàïÿòàÿ óæå áûëà íàéäåíà 
+		{
+			if (t == l - 1 || k > 0) { a = false; } k++;
+		}
+		//åñëè t-ûé ñèìâîë íå ëåæèò â äèàïàçîíå îò '0' äî '9' 
+		else if (str[t] < '0' ||
+			str[t] > '9') {
+			a = false;
+		}
+		t++;
+	}
+	if (a == false)
+	{
+		MessageBox::Show("ïàðàìåòð À íå ÿâëÿåòñÿ ÷èñëîì", "Îøèáêà ââîäà äàííûõ", MessageBoxButtons::OK, MessageBoxIcon::Exclamation);
+		//âîçâðàò ôîêóñà òåêñòîâîìó ïîëþ 
+		this->textBox1->Focus();
+	}
+}
+};
+}
